173f1a6e06504b429d9d7e663297915eae6647d3
William <bzfishmaster94@gmail.com>
2024-09-15 03:33:32 -0400

    added styling and other stuff

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,12 @@
 const About = () => {
     return (
         <div id="about-div">
+        <div id="container"></div>
             <h2 id="about-title">About Me</h2>
-            <img src=""></img>
-            <p id="about-info">description of about me example</p>
+            <div id="about-section">
+                <img id="profile-img" src="../../public/profile-pic.jpg"></img>
+                <p id="about-info">description of about me example</p>
+            </div>
         </div>
     )
 };
